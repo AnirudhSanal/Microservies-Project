516e3d9b5d32bac3aca5c55894c07cee7aa9347c
AnirudhSanal <147903537+AnirudhSanal@users.noreply.github.com>
2025-01-16 01:59:07 +0530

    Delete Microservices Using Spring Boot/Microservices directory

--- Microservices Using Spring Boot/Microservices/TrainService/src/main/java/com/hotel/service/repositories/HotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.service.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hotel.service.entity.Hotel;
-
-public interface HotelRepository extends JpaRepository<Hotel, String>{
-
-}
